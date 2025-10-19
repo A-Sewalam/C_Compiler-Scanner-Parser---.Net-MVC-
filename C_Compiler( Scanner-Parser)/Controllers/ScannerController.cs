b2f204af967e28cce738b8ca78e3446aecd7c42b
Sewalam <wahhab.salah.20@gmail.com>
2025-10-19 14:43:49 +0300

    add ScannerController

---
+++ C_Compiler( Scanner-Parser)/Controllers/ScannerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace C_Compiler__Scanner_Parser_.Controllers
+{
+    public class ScannerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
