524eff752cb5b8b780577977ed2cbbe592de2e94
Sewalam <wahhab.salah.20@gmail.com>
2025-10-19 10:53:54 +0300

    initial + add token types model + add token model

--- C_Compiler( Scanner-Parser)/Models/Token.cs
+++ C_Compiler( Scanner-Parser)/Models/Token.cs
@@ -1,6 +1,21 @@
 ﻿namespace C_Compiler__Scanner_Parser_.Models
 {
-    public class Token
-    {
-    }
+   
+        public class Token
+        {
+            public TokenType Type { get; set; }
+            public string Value { get; set; }
+
+            public Token(TokenType type, string value)
+            {
+                Type = type;
+                Value = value;
+            }
+
+            public override string ToString()
+            {
+                return $"< {Type}, {Value} >";
+            }
+        }
+    
 }
