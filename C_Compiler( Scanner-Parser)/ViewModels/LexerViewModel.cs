2579febc429c866fe3b899da6f9ac199aa5a382d
Sewalam <wahhab.salah.20@gmail.com>
2025-10-19 14:18:16 +0300

    add ViewModels Folder + add LexerViewModel

---
+++ C_Compiler( Scanner-Parser)/ViewModels/LexerViewModel.cs
@@ -0,0 +1,16 @@
+﻿using C_Compiler__Scanner_Parser_.Models;
+
+namespace C_Compiler__Scanner_Parser_.ViewModels
+{
+    public class LexerViewModel
+    {
+        public string? InputCode { get; set; }
+        public List<Token>? Tokens { get; set; }
+        public string? ErrorMessage { get; set; }
+
+        public LexerViewModel()
+        {
+            Tokens = new List<Token>();
+        }
+    }
+}
